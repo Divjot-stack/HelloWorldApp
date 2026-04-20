654b18e0ed530cb9dc324c7e87f2f3da381b9d0f
Divjot-stack <ds0725@srmist.edu.in>
2026-04-20 18:05:06 +0530

    UC3: Display Hello with default World

--- display-name.java
+++ display-name.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length > 0) {
@@ -8,4 +9,11 @@ public class HelloApp {
         }
 
     }
+=======
+public class HelloApp {
+    public static void main(String[] args) {
+		String name = args[0];
+		System.out.println("Hello, " + name + "!");
+    }
+>>>>>>> c190cb2 (UC1: Display Hello World)
 }
